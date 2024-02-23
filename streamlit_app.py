9c609b345b6e207d4b3a17b103c9bdf66c72d584
Radhika-Bhrara <161001732+Radhika-Bhrara@users.noreply.github.com>
2024-02-23 15:04:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,32 +59,32 @@ elif selection == "Team Description":
     team_members_info = [
         {
             "name": "**Radhika** 👩‍💼",
-            "description": "The Visionary Maestro\nResponsibilities: As the team leader, Radhika orchestrates the team's success by overseeing various critical aspects. From ensuring a sleek and user-friendly Graphical User Interface (GUI) to meticulously collecting and managing code, maintaining Git tickets, and curating an organized repository, Radhika is the backbone of the development process. Not stopping there, she delves into extensive research and documentation, laying the foundation for innovation.\n",
+            "description": "  The Visionary Maestro\nResponsibilities: As the team leader, Radhika orchestrates the team's success by overseeing various critical aspects. From ensuring a sleek and user-friendly Graphical User Interface (GUI) to meticulously collecting and managing code, maintaining Git tickets, and curating an organized repository, Radhika is the backbone of the development process. Not stopping there, she delves into extensive research and documentation, laying the foundation for innovation.\n",
             "url": "https://www.linkedin.com/in/radhika-bhrara/",
         },
         {
-            "name": "**\n\nSushant** 👨‍💻",
-            "description": "The Product Sage\nResponsibilities: Sushant, the Product Sage, brings a keen eye for detail and innovation to the team. He spearheads product research, meticulously finalizes different attributes and parameters, and selects features that not only meet but exceed expectations. With a passion for excellence, Sushant is the driving force behind the product's evolution and its alignment with user needs.\n",
+            "name": "\n\n**Sushant** 👨‍💻",
+            "description": "  The Product Sage\nResponsibilities: Sushant, the Product Sage, brings a keen eye for detail and innovation to the team. He spearheads product research, meticulously finalizes different attributes and parameters, and selects features that not only meet but exceed expectations. With a passion for excellence, Sushant is the driving force behind the product's evolution and its alignment with user needs.\n",
             "url": "https://www.linkedin.com/in/sushant-thombre-050ab8154/",
         },
         {
-            "name": "**\n\nMuzaffar** 🤖",
-            "description": "The Techno-Artisan\nResponsibilities: Muzaffar, the Techno-Artisan, is the team's tech image guru. With an exceptional eye for detail, he excels as the code reviewer and merger, ensuring that the team's work is not only efficient but also elegant. His expertise extends to crop classification, where he utilizes his technical prowess to enhance the precision and reliability of the algorithms at play.\n",
+            "name": "\n\n**Muzaffar** 🤖",
+            "description": "  The Techno-Artisan\nResponsibilities: Muzaffar, the Techno-Artisan, is the team's tech image guru. With an exceptional eye for detail, he excels as the code reviewer and merger, ensuring that the team's work is not only efficient but also elegant. His expertise extends to crop classification, where he utilizes his technical prowess to enhance the precision and reliability of the algorithms at play.\n",
             "url": "https://www.linkedin.com/in/muzaffar-tasgoankar/",
         },
         {
-            "name": "**\n\nPriyanshu** 🔧",
-            "description": "The Integration Alchemist\nResponsibilities: Priyanshu, the Integration Alchemist, is the maestro behind seamless API integration and expertly navigates the realms of web scraping. His tireless research efforts ensure that the team is always on the cutting edge of data acquisition. With a penchant for detail, Priyanshu ensures that data flows seamlessly into the system, supporting the team's goals.\n",
+            "name": "\n\n**Priyanshu** 🔧",
+            "description": "  The Integration Alchemist\nResponsibilities: Priyanshu, the Integration Alchemist, is the maestro behind seamless API integration and expertly navigates the realms of web scraping. His tireless research efforts ensure that the team is always on the cutting edge of data acquisition. With a penchant for detail, Priyanshu ensures that data flows seamlessly into the system, supporting the team's goals.\n",
             "url": "https://www.linkedin.com/in/08-priyanshu-jangir/",
         },
         {
-            "name": "**\n\nPavan** 📊",
-            "description": "The Model Maestro\nResponsibilities: Pavan, the Model Maestro, takes center stage in the intricate world of model loading, training, and the classification of parameters. His expertise is crucial in ensuring that the algorithms employed are not only cutting-edge but also finely tuned to deliver precise results. Pavan's dedication to model excellence is the heartbeat of the team's success.\n",
+            "name": "\n\n**Pavan** 📊",
+            "description": "  The Model Maestro\nResponsibilities: Pavan, the Model Maestro, takes center stage in the intricate world of model loading, training, and the classification of parameters. His expertise is crucial in ensuring that the algorithms employed are not only cutting-edge but also finely tuned to deliver precise results. Pavan's dedication to model excellence is the heartbeat of the team's success.\n",
             "url": "https://www.linkedin.com/in/pawan-balijireddi/",
         },
         {
-            "name": "**\n\nGeethan** 🧑‍🔬",
-            "description": "The Tools Explorer\nResponsibilities: Geethan, the Tools Explorer, embarks on a journey of extensive research and comparison to identify the most effective tools and approaches. His analytical mind is the team's compass, guiding them through the labyrinth of options. From data extraction methods to evaluating tools, Geethan's insights shape the team's strategic decisions and technological advancements.\n",
+            "name": "\n\n**Geethan** 🧑‍🔬",
+            "description": "  The Tools Explorer\nResponsibilities: Geethan, the Tools Explorer, embarks on a journey of extensive research and comparison to identify the most effective tools and approaches. His analytical mind is the team's compass, guiding them through the labyrinth of options. From data extraction methods to evaluating tools, Geethan's insights shape the team's strategic decisions and technological advancements.\n",
             "url": "https://www.linkedin.com/in/geethan-b-899b1a233/",
         },
     ]
